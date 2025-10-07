28cd9be3cfb253c2215cdc3c86b6a0a0a5452313
ankurbadhwania <ankurbadhwania@gmail.com>
2025-10-07 13:47:57 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -2,22 +2,43 @@ const express = require("express")
 const connectDB = require("./config/database");
 const User = require("./models/user")
 const app = express();   // creating application
-
+app.use(express.json())  // converts json from api req to js object
 
 app.post("/signup", async (req, res) => {
-    const user =  new User({
-        firstName  : "ankur",
-        lastName : "yadav",
-        email : "abc@gmail.com",
-        password : "127a3"
-    })
-    // const user = new User(userObj);
+    const user = new User(req.body);
     try{
         await user.save();
     res.send("new user added");
     }
     catch(err){
         res.status(400).send("error user not saved: " + err.message);
+    }
+    
+})
+
+// get a user details
+app.get("/users", async (req, res) => {
+    const userEmail = req.body.email;
+    try{
+        const users = await User.find({email : userEmail});
+        if(users.length === 0) res.status(404).send("user not found");
+        else {res.status(200).send(users);}
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+    
+})
+
+// get all users
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await User.find({});
+        if(users.length === 0) res.status(404).send("no users found");
+        else {res.status(200).send(users);}
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
     }
     
 })
